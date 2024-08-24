b6c6e9207442b158ad4625030a0d0781349bc702
reavisjordan <reavisjordan@gmail.com>
2024-08-23 20:40:33 -0700

    Can now sell wood for coins. Started setting up worker logic

--- IdleGame/Assets/Scripts/ResourceManager.cs
+++ IdleGame/Assets/Scripts/ResourceManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -5,14 +6,17 @@ using UnityEngine.UI;
 public class ResourceManager : MonoBehaviour
 {
     public TextMeshProUGUI woodCount;
-    private int woodCollected = 0;
+    private int wood;
 
     public TextMeshProUGUI coinCount;
+    private int coins;
+
+    private float workSpeed = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        woodCount.text = PlayerPrefs.GetInt("wood").ToString();
+        UpdateUI();
     }
 
     // Update is called once per frame
@@ -21,10 +25,41 @@ public class ResourceManager : MonoBehaviour
         
     }
 
+    public IEnumerator HarvestWood()
+    {
+        GetWood();
+        yield return new WaitForSeconds(workSpeed);
+    }
+
     public void GetWood()
     {
-        woodCollected++;
-        PlayerPrefs.SetInt("wood", woodCollected);
-        woodCount.text = woodCollected.ToString();
+        wood++;
+        PlayerPrefs.SetInt("wood", wood);
+        woodCount.text = wood.ToString();
+    }
+
+    public void SellWood()
+    {
+        coins += wood;
+        PlayerPrefs.SetInt("coins", coins);
+        coinCount.text = coins.ToString();
+
+        wood = 0;
+        PlayerPrefs.SetInt("wood", wood);
+        woodCount.text = wood.ToString();
+    }
+
+    public void ClearUserData()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
+    public void UpdateUI()
+    {
+        wood = PlayerPrefs.GetInt("wood");
+        woodCount.text = PlayerPrefs.GetInt("wood").ToString();
+
+        coins = PlayerPrefs.GetInt("coins");
+        coinCount.text = PlayerPrefs.GetInt("coins").ToString();
     }
 }
